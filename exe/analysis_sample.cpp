2a363bef928193a840e410dba4aebbf39335b005
GiorgioAlbertoLucia <87222843+GiorgioAlbertoLucia@users.noreply.github.com>
2022-04-11 15:05:55 +0200

    initial commit

---
+++ exe/analysis_sample.cpp
@@ -0,0 +1,111 @@
+#include <TH1D.h>
+#include <TString.h>
+#include <TSystem.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TGraphErrors.h>
+#include <TStyle.h>
+#include <TROOT.h>
+
+#include "useful_functions.cpp"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string> 
+
+using namespace std;
+
+void analysis_sample()
+{   
+    ///////////////////// SET OUTPUT IN A FILE AND OTHER GENERAL SETTINGS ///////////////////////
+
+    /*
+    //freopen("../output/analysis1.txt", "w", stdout);
+    gROOT->SetStyle("Plain");
+    gStyle->SetOptFit(1100);
+    gStyle->SetFitFormat("2.2e");
+    */
+
+    ///////////////////// READ DATA FROM A FILE ////////////////////////////////////////////////
+    /* First line is a description of each column */
+    /*
+    ifstream file("../data/catena_elettronica.txt");
+
+    vector<float> Vin, Vout, CHN, err_Vin, err_Vout, err_CHN;
+    float entry1, entry2, entry3, entry4, entry5, entry6;
+    string line;
+    getline(file, line);                                            // skip first line
+
+    while (file >> entry1 >> entry2 >> entry3 >> entry4 >> entry5 >> entry6)
+    {
+        Vin.push_back(entry1);
+        Vout.push_back(entry2);
+        CHN.push_back(entry3);
+        err_Vin.push_back(entry4);
+        err_Vout.push_back(entry5);
+        err_CHN.push_back(entry6);
+    }
+    */
+
+
+    ///////////////////////////// ADD DATA ///////////////////////////////////////////////////////
+    /*
+    for(int i = 0; i < chn_centroid.size(); i++)   
+    {
+        float Ei = a + b * chn_centroid.at(i) + c *chn_centroid.at(i) *chn_centroid.at(i);
+        float err_Ei = sqrt(err_a*err_a + chn_centroid.at(i)*err_b*err_b + chn_centroid.at(i)*chn_centroid.at(i)*err_c*err_c 
+                        + (b + c*chn_centroid.at(i)*err_centroid.at(i)*err_centroid.at(i)));
+        E.push_back(Ei);
+        err_E.push_back(err_Ei);
+    }
+
+    // if there are no columns named E and err_E, respective values are appended to the file
+    string str_E("E"), str_err_E("err_energy");
+    if(line.find(str_E) == string::npos)        append_column(name, "E", E);
+    if(line.find(str_err_E) == string::npos)    append_column(name, "err_energy", err_E);
+    */
+
+    ////////////////////////// CLOSE FILE ///////////////////////////////////////////////////
+    /*
+    file.close();
+    */
+
+
+    ////////////////////////// PRINT OUT DATA /////////////////////////////////////////////////
+    /*
+    cout << endl << "Dati:" << endl << line << endl;
+    for (int i = 0; i < Vin.size(); i++)   
+        cout << Vin.at(i) << "\t" << Vout.at(i) << "\t" << CHN.at(i) << "\t" << err_Vin.at(i) << "\t" << 
+        err_Vout.at(i) << "\t\t" << err_CHN.at(i) << endl;
+    cout << endl;
+    
+    TCanvas * canvas1 = new TCanvas("Vin", "Vout", 500, 5, 500, 600);
+    canvas1->SetGrid();
+    */
+    
+
+    /////////////////////////////// FIT //////////////////////////////////////////////////////
+    /*
+    TF1 * tf1 = new TF1("tf1", "[0]+[1]*x", -15, 15);
+    tf1->SetLineColor(38);
+
+    TGraphErrors * graph1 = new TGraphErrors(Vin.size(), &Vin[0], &Vout[0], &err_Vin[0], &err_Vout[0]);
+    graph1->SetTitle("#splitline{Catena Elettronica}{y = p_{0} + p_{1} x};Vin [V];Vout [V]");
+    graph1->GetYaxis()->SetTitleOffset(1.4);
+    gPad->SetTopMargin(0.15);
+    gPad->SetLeftMargin(0.15);
+    graph1->SetMarkerStyle(21);
+    graph1->SetMarkerSize(0.3);
+    
+    graph1->Fit(tf1, "ER");
+    graph1->Draw("ap");
+    canvas1->SaveAs("../graphs/linearità1.jpg");
+    canvas1->SaveAs("../graphs/linearità1.pdf");
+    
+    cout << "Chi^2:" << tf1->GetChisquare() << ", number of DoF: " << tf1->GetNDF() << 
+    " (Probability: " << tf1->GetProb() << ")." << endl;
+    */
+
+
+}
---
+++ exe/useful_functions.cpp
@@ -0,0 +1,88 @@
+#include <TFile.h>
+#include <TH1D.h>
+#include <TString.h>
+#include <TSystem.h>
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TH1F.h>
+#include <TGraphErrors.h>
+#include <TMath.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+////////////////////////////////// APPEND COLUMN ////////////////////////////////////////
+/**
+ * @brief This function add a column of floats (with their description as top line) to an existing .txt file
+ * @param file_name path to the file
+ * @param col_name description of the column data
+ * @param column data to add in the column
+ */
+void append_column(const char * file_name, const char * col_name, vector<float> column)
+{
+    fstream file(file_name, ios::in);
+    string line;
+    vector<string> file_lines;
+    while(getline(file, line))  file_lines.push_back(line);     // fill a vector with the file content
+    file.close();
+
+    file.open(file_name, ios::out);
+    for (int i = 0; i < file_lines.size(); i++)
+    {
+        if (i == 0)     file << file_lines.at(i) << "\t\t" << col_name << endl;
+        else            file << file_lines.at(i) << "\t\t\t" << column.at(i-1) << endl;                          
+    }
+    file.close();
+}
+
+//////////////////////////////////// Z TEST /////////////////////////////////////////////////////////////
+
+/**
+ * @brief Gaussian test. The function prints the value of the variable Z and the result of the test
+ * @param exp_value 
+ * @param ref_value 
+ * @param err 
+ */
+void z_test(const float exp_value, const float ref_value, const float err)
+{
+    float z = (exp_value - ref_value) / err;
+    if (abs(z) < 1.96)  cout << exp_value << " +/- " << err << "\t" << ref_value << "\t"  << z << "\t" << "True" << endl;
+    else                cout << exp_value << " +/- " << err << "\t" << ref_value << "\t"  << z << "\t" << "False" << endl;
+}
+
+/**
+ * @brief This function takes a file in ../doc (no extension) where a TTree is stored. Executes a Z Test over all the values
+ * stored in a specific branch in respect to the reference values stored in another branch (uncertainties are also stored in
+ * some other branch).
+ * 
+ * @param file_name name of the .root file in ../doc (no extension)
+ * @param exp_branch name of the branch where the experimental values are stored
+ * @param ref_branch name of the branch where the reference values are stored
+ * @param err_branch name of the branch where the experimental uncertainties are stored
+ * @param name_branch name of the branch where the description of the data in the line is stored
+ */
+void z_test_branch(const char * file_name)
+{
+    ifstream file(file_name);
+
+    string name;
+    float entry1, entry2, entry3, ref, exp, err;
+
+    string line;
+    getline(file, line);    // skip first line
+
+    cout << endl << "test z dal file " << file_name << endl;
+    while (file >> name >> entry1 >> entry2 >> entry3 >> ref >> exp >> err)
+    {
+        cout << name << "\t";
+        z_test(exp, ref, err);
+    }
+    file.close();
+}
+
